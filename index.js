33ec11d8a61ac46703698c19e5e70e7ebf4cb088
Abul Kawser <uiu.afser@gmail.com>
2022-11-10 04:56:49 +0600

    database connected successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +27,29 @@ app.use(express.json());
 //     next();
 //   });
 // }
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gt8bmu9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+   const menuCollection = client.db("petChukti").collection('menu');
+  app.get('/menu',async(req,res)=>{
+   
+    const query={};
+    const cursor = menuCollection.find(query);
+    const menuItems = await cursor.toArray();
+    res.send(menuItems);
+    
+  })
+}
+
+run().catch(err=>console.log(err));
 
 
 
