32adcf5f5fb8a9953e4d5462123ec3e8b92759f1
Abul Kawser <uiu.afser@gmail.com>
2022-11-10 06:29:50 +0600

    get menu from id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -57,6 +57,14 @@ async function run(){
     res.send(menuItems);
     
   })
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const menu = await menuCollection.findOne(query);
+      res.send(menu);
+    });
+
 }
 
 run().catch(err=>console.log(err));
